4b570a084b5c1f223d4d625dbaac1adbb17a3dae
nabil16497 <nabil16497@gmail.com>
2022-03-30 01:53:49 +0600

    portfolio and testimonial done

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,88 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/dp.png'
+import IMG2 from '../../assets/dp.png'
+import IMG3 from '../../assets/dp.png'
+import IMG4 from '../../assets/dp.png'
+import IMG5 from '../../assets/dp.png'
+import IMG6 from '../../assets/dp.png'
+
+
+
+const data = [
+  {
+    id:1,
+    image: IMG1,
+    title: "Portfolio Item Title",
+    git: "https://github.com",
+    demo: "https://youtube.com"
+  },
+  {
+    id:2,
+    image: IMG2,
+    title: "Portfolio Item Title",
+    git: "https://github.com",
+    demo: "https://youtube.com"
+  },
+  {
+    id:3,
+    image: IMG3,
+    title: "Portfolio Item Title",
+    git: "https://github.com",
+    demo: "https://youtube.com"
+  },
+  {
+    id:4,
+    image: IMG4,
+    title: "Portfolio Item Title",
+    git: "https://github.com",
+    demo: "https://youtube.com"
+  },
+  {
+    id:5,
+    image: IMG5,
+    title: "Portfolio Item Title",
+    git: "https://github.com",
+    demo: "https://youtube.com"
+  },
+  {
+    id:6,
+    image: IMG6,
+    title: "Portfolio Item Title",
+    git: "https://github.com",
+    demo: "https://youtube.com"
+  }
+]
+
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Works</h5>
+      <h2>Portfolio</h2>
+      
+      <div className="container portfolio__container">
+
+      {
+        data.map(({id, image, title, git, demo}) =>{
+          return(
+            <article key={id} className='portfolio__item'>
+          <div className="portfolio__item-image">
+          <img src={image} alt='' />
+          </div>
+          <h3>{title}</h3>
+          <div className="portfolio__item-cta">
+          <a href={git} className='btn' target='_blank'>Github</a>
+          <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+    
+        </article>
+
+          )
+        })
+      }
+
+      </div>
+    </section>
   )
 }
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,77 @@
 import React from 'react'
 import './testimonials.css'
+import IMG1 from '../../assets/dp.png'
+import IMG2 from '../../assets/dp.png'
+import IMG3 from '../../assets/dp.png'
+import IMG4 from '../../assets/dp.png'
+
+// import Swiper core and required modules
+import {  Pagination} from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+
+
+const data =[
+  {
+    avatar: IMG1,
+    name: 'M M Nabil',
+    review: "IDK. I'll leave it blank for now"
+  },
+  {
+    avatar: IMG2,
+    name: 'M M Nabil',
+    review: "IDK. I'll leave it blank for now"
+  },
+  {
+    avatar: IMG3,
+    name: 'M M Nabil',
+    review: "IDK. I'll leave it blank for now"
+  },
+  {
+    avatar: IMG4,
+    name: 'M M Nabil',
+    review: "IDK. I'll leave it blank for now"
+  }
+]
+
 const Testimonials = () => {
   return (
-    <section id='testimonial'>Testimonials</section>
+    <section id='testimonial'>
+      <h5>Review From Clients</h5>
+      <h2>Testimonials</h2>
+
+
+      <Swiper className="container testimonial__container" 
+       modules={[Pagination]}
+       spaceBetween={40}
+       slidesPerView={1}
+       pagination={{ clickable: true }}
+      >
+        {
+          data.map(({avatar, name, review}, index) => {
+            return(
+              <SwiperSlide key={index} className='testimonial'>
+                <div className="client__avatar">
+                <img src={avatar} alt='' />
+                </div>
+                <h5 className='client__name'>{name}</h5>
+                <small className='client__review'>
+                  {review}
+                </small>
+              </SwiperSlide>
+
+            )
+          })
+        }
+
+      </Swiper>
+    </section>
   )
 }
 
