5c30ec0b3cec930a14eeddb081d5c0a245f238a5
nabil16497 <nabil16497@gmail.com>
2022-05-28 20:57:58 +0600

    test deply2.1

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -12,18 +12,13 @@ import { useState } from 'react'
 
 const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
-  
   return (
     <nav>
-
       <a href='#' onClick={() => setActiveNav('#')} className={activeNav == '#' ? 'active' : ''}><AiOutlineHome /></a>
       <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav == '#about' ? 'active' : ''}><AiOutlineUser /></a>
-      <a href='#services' onClick={() => setActiveNav('#services')} className={activeNav == '#services' ? 'active' : ''}><MdOutlinePlaylistAdd /></a>
-      <a href='#portfolio' onClick={() => setActiveNav('#portfolio')} className={activeNav == '#portfolio' ? 'active' : ''}><AiOutlineAppstoreAdd /></a>
+      <a href='#services' onClick={() => setActiveNav('#services')} className={activeNav == '#services' ? 'active' : ''}><AiOutlineAppstoreAdd /></a>
       <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav == '#contact' ? 'active' : ''}><AiOutlineMessage /></a>
     </nav>
-
-    
   )
 }
 
