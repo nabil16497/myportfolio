fad9c427a0e1cc7907d87f770b4c96567a4721b0
nabil16497 <nabil16497@gmail.com>
2023-09-20 17:35:26 +0600

    compressed images try3

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,66 +1,25 @@
-import React, { useState } from 'react';
-import './nav.css';
-import { AiOutlineHome } from 'react-icons/ai';
-import { AiOutlineUser } from 'react-icons/ai';
-import { MdPlaylistAdd } from 'react-icons/md';
-import { AiOutlineMessage } from 'react-icons/ai';
-import { AiOutlineAppstoreAdd } from 'react-icons/ai';
+import React from 'react'
+import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {MdOutlinePlaylistAdd} from 'react-icons/md'
+import {AiOutlineMessage} from 'react-icons/ai'
+import {AiOutlineAppstoreAdd} from 'react-icons/ai'
+import { useState } from 'react'
 
-const Nav = ({ theme, toggleTheme }) => {
-  const [activeNav, setActiveNav] = useState('#');
-
-  const handleNavClick = (navId) => {
-    setActiveNav(navId);
-    toggleTheme(navId);
-  };
+  const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
 
   return (
     <nav>
-      <a
-        href="#"
-        onClick={() => handleNavClick('#')}
-        className={activeNav === '#' ? 'active' : ''}
-      >
-        <div className="nametitle">Home</div>
-        <AiOutlineHome />
-      </a>
-      <a
-        href="#about"
-        onClick={() => handleNavClick('#about')}
-        className={activeNav === '#about' ? 'active' : ''}
-      >
-        <div className="nametitle">About</div>
-        <AiOutlineUser />
-      </a>
-      <a
-        href="#services"
-        onClick={() => handleNavClick('#services')}
-        className={activeNav === '#services' ? 'active' : ''}
-      >
-        <div className="nametitle">Skill</div>
-        <MdPlaylistAdd />
-      </a>
-      <a
-        href="#portfolio"
-        onClick={() => handleNavClick('#portfolio')}
-        className={activeNav === '#portfolio' ? 'active' : ''}
-      >
-        <div className="nametitle">Project</div>
-        <AiOutlineAppstoreAdd />
-      </a>
-      <a
-        href="#contact"
-        onClick={() => handleNavClick('#contact')}
-        className={activeNav === '#contact' ? 'active' : ''}
-      >
-        <div className="nametitle">Contact</div>
-        <AiOutlineMessage />
-      </a>
-      <button className="theme-toggle" onClick={toggleTheme}>
-        Toggle Theme
-      </button>
+      
+      <a href='#' onClick={() => setActiveNav('#')} className={activeNav == '#' ? 'active' : ''}><div className='nametitle'>Home</div><AiOutlineHome /></a>
+      <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav == '#about' ? 'active' : ''}><div className='nametitle'>About</div><AiOutlineUser /></a>
+      <a href='#services' onClick={() => setActiveNav('#services')} className={activeNav == '#services' ? 'active' : ''}><div className='nametitle'>Skill</div><MdOutlinePlaylistAdd /></a>
+      <a href='#portfolio' onClick={() => setActiveNav('#portfolio')} className={activeNav == '#portfolio' ? 'active' : ''}><div className='nametitle'>Project</div><AiOutlineAppstoreAdd /></a>
+      <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav == '#contact' ? 'active' : ''}><div className='nametitle'>Contact</div><AiOutlineMessage /></a>
     </nav>
-  );
-};
+  )
+}
 
-export default Nav;
+export default Nav
