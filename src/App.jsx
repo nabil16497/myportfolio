3dc376361b1d5703e30954ab50ed2a7cdb372663
nabil16497 <nabil16497@gmail.com>
2023-09-20 16:54:53 +0600

    compressed images try2

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,23 @@
-import React, { useState } from 'react';
-import './App.css';
-import Header from './components/header/Header';
-import Nav from './components/nav/Nav';
-import About from './components/about/About';
-import ExtraActivities from './components/extracurricularactivities/ExtracurricularActivities';
-import Services from './components/services/Services';
-import Certification from './components/certification/Certification';
-import Contact from './components/contact/Contact';
-import Footer from './components/footer/Footer';
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import ExtraActivities from './components/extracurricularactivities/ExtracurricularActivities'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Certification from './components/certification/Certification'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 const App = () => {
-  const [theme, setTheme] = useState('dark');
-
-  const toggleTheme = () => {
-    const newTheme = theme === 'dark' ? 'light' : 'dark';
-    setTheme(newTheme);
-    document.documentElement.setAttribute('data-theme', newTheme);
-  };
 
   return (
     <>
       <Header />
-      <Nav theme={theme} toggleTheme={toggleTheme} />
+      <Nav />
       <About />
       <Services />
-      {/*<Portfolio />*/}
+      <Portfolio />
       <ExtraActivities />
       <Certification />
       <Contact />
@@ -33,4 +26,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default App
